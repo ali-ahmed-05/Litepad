13044cd8689b9bce1d92f3dbae12c2bf4af617fa
MutahirPluto <96816612+MutahirPluto@users.noreply.github.com>
2022-04-22 03:50:33 +0500

    Updated

--- frontend/src/contract/addresses.js
+++
@@ -1,8 +0,0 @@
-
- export const zpad_addr = "0x4300536b909FbA47e042fCa31B97c09F64643110"
- export const staking_addr = "0x19Bf1e02478B6FFd9607D12c5AcE6cDbdD4D00e7"
- export const rewardToken_addr = "0x8467806e70FbE05Ca5e17f5d316C09F5bD2391bC"
- export const tokenForSale_addr = "0x8Fb529e2982695d536336EE1b44B66F3Fd5769e8"
- export const ticketConsumer_addr = "0x7499c695c0edA7bA880CeE197dA0175425162a43"
- export const factory_addr = "0x6574F3dea1EB56b9F2e752cB93b7Cc8739176cd5"
- export const busd_addr = "0xe3765f851977Ed7B377D0234e9275845fc960775"
